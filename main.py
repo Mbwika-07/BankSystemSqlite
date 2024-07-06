b18f4e90e137345faec2ac3de96ed105ff5ff7de
Mbwika-07 <allanmbwiks@gmail.com>
2024-07-07 00:14:54 +0300

    Added a new user sign up function

--- main.py
+++ main.py
@@ -15,9 +15,19 @@ def login_user(connection):
     else:
         print("Invalid username or password. Please try again.")
 
-
-
-
+def signup_newuser(connection):
+    print("----------------------------------------------------------------------------------------------------------------------------------------------")
+    print("\t\t\t\t\t\tCREATE NEW ACCOUNT")
+    print("----------------------------------------------------------------------------------------------------------------------------------------------")
+    
+    username = input("Enter your username: ")
+    id = input("Enter you national ID number: ")
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    
+    database.add_user(connection, username, id, email, password, 0.0)
+    
+    print("Account created successfully. Try to log in later as the admin has to approve your account.")
 
 
 
@@ -38,14 +48,17 @@ def main():
         print("WELCOME TO THE NATIONAL BANK PROGRAM. HOW WOULD YOU LIKE TO PROCEED?")
         print("\t1. CUSTOMER LOGIN PAGE")
         print("\t2. ADMIN LOGIN PAGE")
-        print("\t3. EXIT")
-        choice = input("KINDLY ENTER YOUR CHOICE(1-3): ")
+        print("\t3. SIGN IN NEW USER.")
+        print("\t4. EXIT")
+        choice = input("KINDLY ENTER YOUR CHOICE(1-4): ")
         
         if choice == "1":
             login_user(connection)
         elif choice == "2":
             pass
         elif choice == "3":
+            signup_newuser(connection)
+        elif choice == "4":
             is_running = False
         else:
             print("INVALID CHOICE")
